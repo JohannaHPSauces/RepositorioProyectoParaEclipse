f1cfb4a3c2dafe1e2ec307f55c485c1f8a4c3933
Formacion <Formacion@serbatic-07336.VASS.INET>
2022-11-16 12:56:02 +0100

    Nueva clase

---
+++ src/modelo/MostrarPersona.java
@@ -0,0 +1,13 @@
+package modelo;
+
+public class MostrarPersona {
+
+	public static void main(String[] args) {
+		Persona p= new Persona("Elena","pozuelo",42);
+		
+		System.out.print(p.toString());
+
+
+	}
+
+}
--- src/modelo/Persona.java
+++ src/modelo/Persona.java
@@ -2,11 +2,14 @@ package modelo;
 
 public class Persona {
 	private String nombre;
+	private String apellido1;
 	private int edad;
-
-	public Persona(String nombre) {
+	
+	public Persona(String nombre, String apellido1, int edad) {
 		super();
 		this.nombre = nombre;
+		this.apellido1 = apellido1;
+		this.edad = edad;
 	}
 
 	public String getNombre() {
@@ -14,14 +17,24 @@ public class Persona {
 	}
 
 	public void setNombre(String nombre) {
-		this.nombre= nombre;
+		this.nombre = nombre;
 	}
 
-	@Override
-	public String toString() {
-		return "Persona [nombre=" + nombre + "]";
+	public String getApellido1() {
+		return apellido1;
 	}
-	
+
+	public void setApellido1(String apellido1) {
+		this.apellido1 = apellido1;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
 	
 
 }
